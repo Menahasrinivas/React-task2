110e45d98719ecabf62a15b2bd84ecb9f944a1e3
Menahasrinivas <menahasrinivas@gmail.com>
2023-12-21 22:29:04 +0530

    Update

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,65 +1,51 @@
 import React,{useState} from 'react'
-const Card = ({ data, count, setCount }) => {
-  let [btnText, setBtnText] = useState("Add to Cart");
 
-  return (
-    <>
-      <div className="col mb-5">
-        <div className="card h-100">
-          {data.sales ? (
-            <>
-              <div
-                className="badge bg-dark text-white position-absolute"
-                style={{ top: "0.5rem", right: "0.5rem" }}
-              >
-                Sale
+function Card({cart,setCart,product}) {
+    let [outCart,setOutCart]=useState(true)
+    
+  return <div className="col mb-5">
+  <div className="card h-100">
+      {product.sale?(<><div className="badge bg-dark text-white position-absolute" style={{top: "0.5rem", right: "0.5rem"}}>Sale</div></>):("")}
+      <img className="card-img-top" src={product.image} alt="..." />
+      <div className="card-body p-4">
+          <div className="text-center">
+              <h5 className="fw-bolder">{product.name}</h5>
+              {product.reviews?(<><div className="d-flex justify-content-center small text-warning mb-2">
+                  <div className="bi-star-fill"></div>
+                  <div className="bi-star-fill"></div>
+                  <div className="bi-star-fill"></div>
+                  <div className="bi-star-fill"></div>
+                  <div className="bi-star-fill"></div>
               </div>
-            </>
-          ) : (
-            ""
-          )}
-          <img
-            className="card-img-top"
-            src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg"
-            alt="..."
-          />
-          <div className="card-body p-4">
-            <div className="text-center">
-              <h5 className="fw-bolder">{data.productName}</h5>
-              <div className="d-flex justify-content-center small text-warning mb-2">
-                {data.review ? (
-                  <>
-                    <div className="bi-star-fill"></div>
-                    <div className="bi-star-fill"></div>
-                    <div className="bi-star-fill"></div>
-                    <div className="bi-star-fill"></div>
-                    <div className="bi-star-fill"></div>
-                  </>
-                ) : (
-                  ""
-                )}
-              </div>
-              <span
-                className={
-                  data.countisSalePrice
-                    ? ""
-                    : "text-muted text-decoration-line-through"
-                }
-              >
-                {data.salePrice}
-              </span>
-              {data.Price}
-            </div>
+              </>):("")}
+              {product.original ? (
+  <div>
+    <span className="text-muted text-decoration-line-through">{product.originalPrice}</span>
+      {product.price}
+  </div>
+) : (
+  product.price
+)}
+
           </div>
-          <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
-            <div className="text-center">
-            <button className="btn btn-outline-dark mt-auto" onClick={() => {btnText === "Add to Cart"? `${setBtnText("Remove from Cart")} ${setCount(count + 1)}`: `${setBtnText("Add to Cart")} ${setCount(count - 1)}`}}>{btnText}</button>
-            </div>
-          </div>
-        </div>
       </div>
-    </>
-  );
-};
+      <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                <div className="text-center">
+                                    {
+                                        outCart?<button className="btn btn-outline-success mt-auto" href="#" onClick={()=>{
+                                            setCart(cart+1);
+                                            setOutCart(false);
+                                        }}>Add to Cart</button>:
+                                        <button className="btn btn-outline-danger mt-auto" href="#" onClick={()=>{
+                                            setCart(cart-1);
+                                            setOutCart(true);
+                                        }}>Remove from cart</button>
+                                    }
+                                </div>
+                            </div>
+    </div>
+</div>      
 
-export default Card;
+}
+
+export default Card
--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,9 +1,7 @@
 import React from 'react'
 
-const TopBar({cart,setCart})=> {
-  return (
-  <>
-  <nav className="navbar navbar-expand-lg navbar-light bg-light">
+function TopBar({cart,setCart}) {
+  return <nav className="navbar navbar-expand-lg navbar-light bg-light">
   <div className="container px-4 px-lg-5">
       <a className="navbar-brand" href="#!">Start Bootstrap</a>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
@@ -31,8 +29,6 @@ const TopBar({cart,setCart})=> {
       </div>
   </div>
 </nav>
-</>
-)
 }
 
 export default TopBar
